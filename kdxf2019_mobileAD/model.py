58d5d892b997e8b5bd797e5c10b0c9e736fdcdd0
tusonggao <tusonggao@163.com>
2019-07-27 17:25:20 +0800

    update

---
+++ kdxf2019_mobileAD/model.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+
+df1 = pd.read_csv('../data/round1_iflyad_anticheat_traindata.txt')
+
+df2 = pd.read_csv('../data/round1_iflyad_anticheat_testdata_feature.txt')
