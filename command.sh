b7811903dba1723c9ca9035073f1857aa402498d
tusonggao <tusonggao@163.com>
2019-12-27 10:04:52 +0800

    add command line file

---
+++ command.sh
@@ -0,0 +1,3 @@
+kaggle competitions submit -c [COMPETITION NAME] -f [FILE PATH]
+kaggle  competitions  submit  -c  ashrae-energy-prediction  -f  ./submission/submission_newest.7z  -m 'first submission'
+7z a submission_newest.7z  submission_newest.csv
