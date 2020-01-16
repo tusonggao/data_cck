8fc140766dac39838857b87cf5efe04db77b8fe4
tusonggao <tusonggao@163.com>
2020-01-16 12:03:58 +0800

    update files

--- quora-insincere-questions-classification/keras_attention_model.py
+++ quora-insincere-questions-classification/keras_attention_model.py
@@ -338,7 +338,8 @@ def train_pred(model, epochs=2):
 
 print('get here 444')
 start_t = time.time()
-pred_val_y, pred_test_y = train_pred(model, epochs=15)
+#pred_val_y, pred_test_y = train_pred(model, epochs=15)
+pred_val_y, pred_test_y = train_pred(model, epochs=2)
 print('train_pred cost time time: ', time.time() - start_t)
 
 '''
