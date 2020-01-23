5422e794c9a0695655824a538cc3524a7aa47b87
tusonggao <tusonggao@163.com>
2020-01-23 17:13:36 +0800

    add files

--- kkbox-music-recommendation-challenge/model_tsg.py
+++ kkbox-music-recommendation-challenge/model_tsg.py
@@ -9,8 +9,10 @@ import pandas as pd
 
 df = pd.read_csv('./atad/train.csv')
 print('df.shape is ', df.shape)
+# df.shape is  (7377418, 6)
 
 print('df.head(10) is ', df.head(10))
 
 print('df.target 1 ratio is ', df.target.sum()/len(df))
+# df.target 1 ratio is  0.5035170841614234
 
