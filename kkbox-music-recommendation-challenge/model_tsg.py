dcb2060bfed9ca00181a9ebac954aeedbc3b7909
tusonggao <tusonggao@163.com>
2020-01-24 21:13:16 +0800

    update files

--- kkbox-music-recommendation-challenge/model_tsg.py
+++ kkbox-music-recommendation-challenge/model_tsg.py
@@ -11,9 +11,25 @@ train_df = pd.read_csv('./atad/train.csv')  # df.shape is  (7377418, 6)   df.tar
 print('train_df.shape is ', train_df.shape)
 print('train_df.head(10) is ', train_df.head(10))
 print('train_df.target 1 ratio is ', train_df.target.sum()/len(train_df))
-print('train_df.source_system_tab nunique is ', train_df.target.sum()/len(train_df))
+print('train_df.source_system_tab nunique is ', train_df['source_system_tab'].nunique())
+print('train_df.source_screen_name nunique is ', train_df['source_screen_name'].nunique())
+print('train_df.source_type nunique is ', train_df['source_type'].nunique())
 #source_system_tab,source_screen_name,source_type
 
+
+#test_df = pd.read_csv('./atad/test.csv')
+#print('test_df.shape is ', test_df.shape)
+
+test_df = pd.read_csv('./atad/test.csv')
+print('test_df.shape is ', test_df.shape)
+
+members_df = pd.read_csv('./atad/members.csv')
+print('members_df.shape is ', members_df.shape)
+print('members_df.head(10) is ', members_df.head(10))
+
+song_df = pd.read_csv('./atad/songs.csv')
+print('song_df.shape is ', song_df.shape)
+print('song_df.head(10) is ', song_df.head(10))
 
 song_extra_info_df = pd.read_csv('./atad/song_extra_info.csv')
 print('song_extra_info_df.shape is ', song_extra_info_df.shape)
